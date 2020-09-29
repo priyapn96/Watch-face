83e2400057f98c86f25dcc1216b56a65b805ae45
azar.sheik <azar.sheik@codecraft.co.in>
2020-09-29 21:02:06 +0530

    initial commit

---
+++ app/src/main/java/com/example/watchfacesample/MyWatchFace.kt
@@ -0,0 +1,492 @@
+package com.example.watchfacesample
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Rect
+import android.os.Bundle
+import android.os.Handler
+import android.os.Message
+import android.support.wearable.watchface.CanvasWatchFaceService
+import android.support.wearable.watchface.WatchFaceService
+import android.support.wearable.watchface.WatchFaceStyle
+import android.text.TextPaint
+import android.view.SurfaceHolder
+import java.lang.ref.WeakReference
+import java.text.SimpleDateFormat
+import java.util.*
+
+/**
+ * Updates rate in milliseconds for interactive mode. We update once a second to advance the
+ * second hand.
+ */
+private const val INTERACTIVE_UPDATE_RATE_MS = 1000
+
+private const val MSG_UPDATE_TIME = 0
+
+private const val HOUR_STROKE_WIDTH = 5f
+private const val MINUTE_STROKE_WIDTH = 3f
+private const val SECOND_TICK_STROKE_WIDTH = 2f
+
+private const val CENTER_GAP_AND_CIRCLE_RADIUS = 4f
+
+private const val SHADOW_RADIUS = 6f
+
+
+class MyWatchFace : CanvasWatchFaceService() {
+
+    override fun onCreateEngine(): Engine {
+        return Engine()
+    }
+
+    private class EngineHandler(reference: MyWatchFace.Engine) : Handler() {
+        private val mWeakReference: WeakReference<MyWatchFace.Engine> = WeakReference(reference)
+
+        override fun handleMessage(msg: Message) {
+            val engine = mWeakReference.get()
+            if (engine != null) {
+                when (msg.what) {
+                    MSG_UPDATE_TIME -> engine.handleUpdateTimeMessage()
+                }
+            }
+        }
+    }
+
+    inner class Engine : CanvasWatchFaceService.Engine() {
+
+        private lateinit var mCalendar: Calendar
+
+        private var mRegisteredTimeZoneReceiver = false
+        private var mMuteMode: Boolean = false
+        private var mCenterX: Float = 0F
+        private var mCenterY: Float = 0F
+
+        private var mSecondHandLength: Float = 0F
+        private var sMinuteHandLength: Float = 0F
+        private var sHourHandLength: Float = 0F
+
+        /* Colors for all hands (hour, minute, seconds, ticks) based on photo loaded. */
+        private var mWatchHandColor: Int = 0
+        private var mWatchHandHighlightColor: Int = 0
+        private var mWatchHandShadowColor: Int = 0
+
+        private lateinit var mHourPaint: Paint
+        private lateinit var mMinutePaint: Paint
+        private lateinit var mSecondPaint: Paint
+        private lateinit var mTickAndCirclePaint: Paint
+        private lateinit var mTickHighlightPaint: Paint
+        private lateinit var mDatePaint: TextPaint
+
+        private lateinit var mBackgroundPaint: Paint
+//        private lateinit var mBackgroundBitmap: Bitmap
+//        private lateinit var mGrayBackgroundBitmap: Bitmap
+
+        private var mAmbient: Boolean = false
+        private var mLowBitAmbient: Boolean = false
+        private var mBurnInProtection: Boolean = false
+
+        private lateinit var mBackGroundColorList: ArrayList<Int>
+        private var mColorIndex: Int = 0
+
+        /* Handler to update the time once a second in interactive mode. */
+        private val mUpdateTimeHandler = EngineHandler(this)
+
+        private val mTimeZoneReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                mCalendar.timeZone = TimeZone.getDefault()
+                invalidate()
+            }
+        }
+
+        override fun onCreate(holder: SurfaceHolder) {
+            super.onCreate(holder)
+            mBackGroundColorList = arrayListOf(
+                Color.parseColor("#FF5733"),
+                Color.parseColor("#33FF83"),
+                Color.parseColor("#FF8D33"),
+                Color.parseColor("#E633FF"),
+                Color.parseColor("#6E33FF")
+            )
+            setWatchFaceStyle(
+                WatchFaceStyle.Builder(this@MyWatchFace)
+                    .setAcceptsTapEvents(true)
+                    .build()
+            )
+
+            mCalendar = Calendar.getInstance()
+
+            initializeBackground()
+            initializeWatchFace()
+        }
+
+        private fun initializeBackground() {
+            mBackgroundPaint = Paint().apply {
+                color = Color.BLACK
+            }
+        }
+
+        private fun initializeWatchFace() {
+            /* Set defaults for colors */
+            mWatchHandColor = Color.BLACK
+            mWatchHandHighlightColor = Color.WHITE
+            mWatchHandShadowColor = Color.BLACK
+
+            mHourPaint = Paint().apply {
+                color = mWatchHandColor
+                strokeWidth = HOUR_STROKE_WIDTH
+                isAntiAlias = true
+                strokeCap = Paint.Cap.ROUND
+                setShadowLayer(
+                    SHADOW_RADIUS, 0f, 0f, mWatchHandShadowColor
+                )
+            }
+
+            mMinutePaint = Paint().apply {
+                color = mWatchHandColor
+                strokeWidth = MINUTE_STROKE_WIDTH
+                isAntiAlias = true
+                strokeCap = Paint.Cap.ROUND
+                setShadowLayer(
+                    SHADOW_RADIUS, 0f, 0f, mWatchHandShadowColor
+                )
+            }
+
+            mSecondPaint = Paint().apply {
+                color = mWatchHandHighlightColor
+                strokeWidth = SECOND_TICK_STROKE_WIDTH
+                isAntiAlias = true
+                strokeCap = Paint.Cap.ROUND
+                setShadowLayer(
+                    SHADOW_RADIUS, 0f, 0f, mWatchHandShadowColor
+                )
+            }
+
+            mTickAndCirclePaint = Paint().apply {
+                color = mWatchHandHighlightColor
+                strokeWidth = SECOND_TICK_STROKE_WIDTH
+                isAntiAlias = true
+                style = Paint.Style.STROKE
+                setShadowLayer(
+                    SHADOW_RADIUS, 0f, 0f, mWatchHandShadowColor
+                )
+            }
+
+            mTickHighlightPaint = Paint().apply {
+                color = mWatchHandHighlightColor
+                strokeWidth = HOUR_STROKE_WIDTH
+                isAntiAlias = true
+                style = Paint.Style.STROKE
+                setShadowLayer(
+                    SHADOW_RADIUS, 0f, 0f, mWatchHandShadowColor
+                )
+            }
+
+            mDatePaint = TextPaint().apply {
+                color = Color.WHITE
+                bgColor = Color.BLACK
+                textSize = 18f
+                style = Paint.Style.FILL
+                isAntiAlias = true
+            }
+        }
+
+        override fun onDestroy() {
+            mUpdateTimeHandler.removeMessages(MSG_UPDATE_TIME)
+            super.onDestroy()
+        }
+
+        override fun onPropertiesChanged(properties: Bundle) {
+            super.onPropertiesChanged(properties)
+            mLowBitAmbient = properties.getBoolean(
+                WatchFaceService.PROPERTY_LOW_BIT_AMBIENT, false
+            )
+            mBurnInProtection = properties.getBoolean(
+                WatchFaceService.PROPERTY_BURN_IN_PROTECTION, false
+            )
+        }
+
+        override fun onTimeTick() {
+            super.onTimeTick()
+            invalidate()
+        }
+
+        override fun onAmbientModeChanged(inAmbientMode: Boolean) {
+            super.onAmbientModeChanged(inAmbientMode)
+            mAmbient = inAmbientMode
+
+            updateWatchHandStyle()
+
+            // Check and trigger whether or not timer should be running (only
+            // in active mode).
+            updateTimer()
+        }
+
+        private fun updateWatchHandStyle() {
+            if (mAmbient) {
+                mHourPaint.color = Color.WHITE
+                mMinutePaint.color = Color.WHITE
+                mSecondPaint.color = Color.WHITE
+                mTickAndCirclePaint.color = Color.WHITE
+                mTickHighlightPaint.color = Color.WHITE
+
+                mHourPaint.isAntiAlias = false
+                mMinutePaint.isAntiAlias = false
+                mSecondPaint.isAntiAlias = false
+                mTickAndCirclePaint.isAntiAlias = false
+                mTickHighlightPaint.isAntiAlias = false
+                mDatePaint.isAntiAlias = false
+
+                mHourPaint.clearShadowLayer()
+                mMinutePaint.clearShadowLayer()
+                mSecondPaint.clearShadowLayer()
+                mTickAndCirclePaint.clearShadowLayer()
+                mTickHighlightPaint.clearShadowLayer()
+
+            } else {
+                mHourPaint.color = mWatchHandColor
+                mMinutePaint.color = mWatchHandColor
+                mSecondPaint.color = mWatchHandHighlightColor
+                mTickAndCirclePaint.color = mWatchHandHighlightColor
+                mTickHighlightPaint.color = mWatchHandHighlightColor
+
+                mHourPaint.isAntiAlias = true
+                mMinutePaint.isAntiAlias = true
+                mSecondPaint.isAntiAlias = true
+                mTickAndCirclePaint.isAntiAlias = true
+                mTickHighlightPaint.isAntiAlias = true
+                mDatePaint.isAntiAlias = true
+
+                mHourPaint.setShadowLayer(
+                    SHADOW_RADIUS, 0f, 0f, mWatchHandShadowColor
+                )
+                mMinutePaint.setShadowLayer(
+                    SHADOW_RADIUS, 0f, 0f, mWatchHandShadowColor
+                )
+                mSecondPaint.setShadowLayer(
+                    SHADOW_RADIUS, 0f, 0f, mWatchHandShadowColor
+                )
+                mTickAndCirclePaint.setShadowLayer(
+                    SHADOW_RADIUS, 0f, 0f, mWatchHandShadowColor
+                )
+                mTickHighlightPaint.setShadowLayer(
+                    SHADOW_RADIUS, 0f, 0f, mWatchHandShadowColor
+                )
+            }
+        }
+
+        override fun onInterruptionFilterChanged(interruptionFilter: Int) {
+            super.onInterruptionFilterChanged(interruptionFilter)
+            val inMuteMode = interruptionFilter == WatchFaceService.INTERRUPTION_FILTER_NONE
+
+            /* Dim display in mute mode. */
+            if (mMuteMode != inMuteMode) {
+                mMuteMode = inMuteMode
+                mHourPaint.alpha = if (inMuteMode) 100 else 100
+                mMinutePaint.alpha = if (inMuteMode) 100 else 100
+                mSecondPaint.alpha = if (inMuteMode) 80 else 100
+                invalidate()
+            }
+        }
+
+        override fun onSurfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
+            super.onSurfaceChanged(holder, format, width, height)
+
+            mCenterX = width / 2f
+            mCenterY = height / 2f
+
+            mSecondHandLength = (mCenterX * 0.875).toFloat()
+            sMinuteHandLength = (mCenterX * 0.75).toFloat()
+            sHourHandLength = (mCenterX * 0.5).toFloat()
+
+        }
+
+        override fun onTapCommand(tapType: Int, x: Int, y: Int, eventTime: Long) {
+            when (tapType) {
+                WatchFaceService.TAP_TYPE_TOUCH -> {
+                    // The user has started touching the screen.
+                }
+                WatchFaceService.TAP_TYPE_TOUCH_CANCEL -> {
+                    // The user has started a different gesture or otherwise cancelled the tap.
+                }
+                WatchFaceService.TAP_TYPE_TAP -> {
+                    // The user has completed the tap gesture.
+                    mColorIndex = (++mColorIndex) % mBackGroundColorList.size
+                }
+            }
+            invalidate()
+        }
+
+
+        override fun onDraw(canvas: Canvas, bounds: Rect) {
+            val now = System.currentTimeMillis()
+            mCalendar.timeInMillis = now
+
+            drawBackground(canvas)
+            drawWatchFace(canvas)
+        }
+
+        private fun drawBackground(canvas: Canvas) {
+
+            if (mAmbient && (mLowBitAmbient || mBurnInProtection)) {
+                canvas.drawColor(Color.BLACK)
+            } else if (mAmbient) {
+                // canvas.drawBitmap(mGrayBackgroundBitmap, 0f, 0f, mBackgroundPaint)
+                canvas.drawColor(Color.BLACK)
+            } else {
+                // canvas.drawBitmap(mBackgroundBitmap, 0f, 0f, mBackgroundPaint)
+                canvas.drawColor(mBackGroundColorList[mColorIndex])
+            }
+        }
+
+        private fun drawWatchFace(canvas: Canvas) {
+
+            val outerTickRadius = mCenterX
+
+            if (!mAmbient) {
+                for (tickIndex in 0..11) {
+                    val innerTickRadius = if (tickIndex % 3 == 0) mCenterX - 20 else mCenterX - 10
+                    val tickRot = (tickIndex.toDouble() * Math.PI * 2.0 / 12).toFloat()
+                    val innerX = Math.sin(tickRot.toDouble()).toFloat() * innerTickRadius
+                    val innerY = (-Math.cos(tickRot.toDouble())).toFloat() * innerTickRadius
+                    val outerX = Math.sin(tickRot.toDouble()).toFloat() * outerTickRadius
+                    val outerY = (-Math.cos(tickRot.toDouble())).toFloat() * outerTickRadius
+
+                    canvas.drawLine(
+                        mCenterX + innerX,
+                        mCenterY + innerY,
+                        mCenterX + outerX,
+                        mCenterY + outerY,
+                        if (tickIndex % 3 == 0) mTickHighlightPaint else mTickAndCirclePaint
+                    )
+                }
+            }
+
+            val seconds =
+                mCalendar.get(Calendar.SECOND) + mCalendar.get(Calendar.MILLISECOND) / 1000f
+            val secondsRotation = seconds * 6f
+
+            val minutesRotation = mCalendar.get(Calendar.MINUTE) * 6f
+
+            val hourHandOffset = mCalendar.get(Calendar.MINUTE) / 2f
+            val hoursRotation = mCalendar.get(Calendar.HOUR) * 30 + hourHandOffset
+
+            /*
+             * Save the canvas state before we can begin to rotate it.
+             */
+
+            val dateText = SimpleDateFormat("EEE  dd").format(mCalendar.time).toUpperCase()
+            val textBound = Rect()
+            mDatePaint.getTextBounds(dateText, 0, dateText.length, textBound)
+            canvas.drawText(
+                dateText,
+                (3 * mCenterX / 2F - textBound.centerX()),
+                mCenterY-textBound.centerY(),
+                mDatePaint)
+
+            canvas.save()
+
+            canvas.rotate(hoursRotation, mCenterX, mCenterY)
+            canvas.drawLine(
+                mCenterX,
+                mCenterY - CENTER_GAP_AND_CIRCLE_RADIUS,
+                mCenterX,
+                mCenterY - sHourHandLength,
+                mHourPaint
+            )
+
+            canvas.rotate(minutesRotation - hoursRotation, mCenterX, mCenterY)
+            canvas.drawLine(
+                mCenterX,
+                mCenterY - CENTER_GAP_AND_CIRCLE_RADIUS,
+                mCenterX,
+                mCenterY - sMinuteHandLength,
+                mMinutePaint
+            )
+
+            if (!mAmbient) {
+                canvas.rotate(secondsRotation - minutesRotation, mCenterX, mCenterY)
+                canvas.drawLine(
+                    mCenterX,
+                    mCenterY - CENTER_GAP_AND_CIRCLE_RADIUS,
+                    mCenterX,
+                    mCenterY - mSecondHandLength,
+                    mSecondPaint
+                )
+
+            }
+            canvas.drawCircle(
+                mCenterX,
+                mCenterY,
+                CENTER_GAP_AND_CIRCLE_RADIUS,
+                mTickAndCirclePaint
+            )
+
+            /* Restore the canvas' original orientation. */
+            canvas.restore()
+        }
+
+        override fun onVisibilityChanged(visible: Boolean) {
+            super.onVisibilityChanged(visible)
+
+            if (visible) {
+                registerReceiver()
+                /* Update time zone in case it changed while we weren't visible. */
+                mCalendar.timeZone = TimeZone.getDefault()
+                invalidate()
+            } else {
+                unregisterReceiver()
+            }
+
+            /* Check and trigger whether or not timer should be running (only in active mode). */
+            updateTimer()
+        }
+
+        private fun registerReceiver() {
+            if (mRegisteredTimeZoneReceiver) {
+                return
+            }
+            mRegisteredTimeZoneReceiver = true
+            val filter = IntentFilter(Intent.ACTION_TIMEZONE_CHANGED)
+            this@MyWatchFace.registerReceiver(mTimeZoneReceiver, filter)
+        }
+
+        private fun unregisterReceiver() {
+            if (!mRegisteredTimeZoneReceiver) {
+                return
+            }
+            mRegisteredTimeZoneReceiver = false
+            this@MyWatchFace.unregisterReceiver(mTimeZoneReceiver)
+        }
+
+
+        private fun updateTimer() {
+            mUpdateTimeHandler.removeMessages(MSG_UPDATE_TIME)
+            if (shouldTimerBeRunning()) {
+                mUpdateTimeHandler.sendEmptyMessage(MSG_UPDATE_TIME)
+            }
+        }
+
+
+        private fun shouldTimerBeRunning(): Boolean {
+            return isVisible && !mAmbient
+        }
+
+        /**
+         * Handle updating the time periodically in interactive mode.
+         */
+        fun handleUpdateTimeMessage() {
+            invalidate()
+            if (shouldTimerBeRunning()) {
+                val timeMs = System.currentTimeMillis()
+                val delayMs = INTERACTIVE_UPDATE_RATE_MS - timeMs % INTERACTIVE_UPDATE_RATE_MS
+                mUpdateTimeHandler.sendEmptyMessageDelayed(MSG_UPDATE_TIME, delayMs)
+            }
+        }
+    }
+}
+
+
